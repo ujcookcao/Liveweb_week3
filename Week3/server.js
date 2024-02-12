fd0bf7e27ddfcaa621f11bc535628340a90bc35c
an cao <an@cs.ucsb.edu>
2024-02-12 16:57:04 -0500

    the index.html is not recongnized from public dir

--- Week3/server.js
+++ Week3/server.js
@@ -1,9 +1,19 @@
+var https = require('https');
+var fs = require('fs'); // Using the filesystem module
+var credentials = { 
+    key: fs.readFileSync('/etc/letsencrypt/live/ac10580.itp.io/privkey.pem'), 
+    cert: fs.readFileSync('/etc/letsencrypt/live/ac10580.itp.io/cert.pem') 
+    };
+
+
+
 const app = require('express')();
-const server = require('http').createServer(app);
+app.use(express.static('public'));
+
+const server = require('https').createServer(credentials,app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-//app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -20,7 +30,10 @@ io.on('connection', socket => {
     });
     socket.on('drawing', (data) => socket.broadcast.emit('draw', data));
     socket.on('Bcolor', (color) => socket.broadcast.emit("Bcolorchange", color));
+    socket.on('erasing', () => socket.broadcast.emit('erase'));
+    socket.on('video', (data) => io.emit('v', data));
 });
 
-//server.listen(80, () => "server started");
-server.listen(3000, () => "server started");
+// var httpsServer = https.createServer(credentials, app);
+server.listen(443, () => "server started");
+//server.listen(3000, () => "server started");
